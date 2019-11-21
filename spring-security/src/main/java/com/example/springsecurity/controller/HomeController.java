0adb30a15182d1744d15d7b9a00e26117bacf51e
liangshuai <1589900136@qq.com>
2019-11-21 14:45:08 +0800

    init

--- spring-security/src/main/java/com/example/springsecurity/controller/HomeController.java
+++ spring-security/src/main/java/com/example/springsecurity/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.example.springsecurity.controller;
 
-import org.hibernate.validator.internal.constraintvalidators.bv.size.SizeValidatorForArraysOfByte;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/index")
-    public String index(){
+    public String index() {
         return "<h1> Welcome Tonels</h1>";
     }
 
     @GetMapping("/user")
-    public String user(){
+    public String user() {
         return "<h1> 普通用户的权限";
     }
 
     @GetMapping("/admin")
-    public String admin(){
+    public String admin() {
         return "<h1> 管理员的的权限";
     }
 
