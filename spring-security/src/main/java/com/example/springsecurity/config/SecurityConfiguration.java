500aa19085bcf5d9bfe8a22142ab7a0da1d4f4a1
liangshuai <1589900136@qq.com>
2019-12-18 15:43:46 +0800

    yml方式配置 springSecurity user

--- spring-security/src/main/java/com/example/springsecurity/config/SecurityConfiguration.java
+++ spring-security/src/main/java/com/example/springsecurity/config/SecurityConfiguration.java
@@ -1,16 +1,16 @@
-//package com.example.springsecurity.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@EnableWebSecurity
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 ////         设置配置 在 auth 对象上
@@ -19,18 +19,33 @@
 //                .and()
 //                .withUser("admin").password("admin").roles("ADMIN");
 //    }
-//
+
+    /**
+     * 此处定义密码加密规则
+     * BCryptPasswordEncoder 默认的是SHA-256 +随机盐+密钥对密码进行加密，采用hash算法，不可逆
+     * todo jasypt可替换加密规则，并提供 加/解 密
+     * @return
+     */
 //    @Bean
-//    public PasswordEncoder getPasswordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
+//    public PasswordEncoder getPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
 //    }
-//
-//   @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/admin").hasRole("ADMIN")
-//                .antMatchers("/user").hasAnyRole("USER","ADMIN")
-//                .antMatchers("/").permitAll()
-//                .and().formLogin();
-//    }
-//}
+
+    /**
+     * 此处配置接口权限
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+
+
+
+}
--- spring-security/src/main/java/com/example/springsecurity/controller/HomeController.java
+++ spring-security/src/main/java/com/example/springsecurity/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.example.springsecurity.controller;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @RestController
 @RequestMapping
@@ -21,6 +27,28 @@ public class HomeController {
     @GetMapping("/admin")
     public String admin() {
         return "<h1> 管理员的的权限";
+    }
+
+    /**
+     * 获取当前用户信息（userName）
+     * 无用户登陆时 返回 anonymousUser
+     *
+     * @param request
+     * @return
+     */
+    @GetMapping("/currentUser")
+    public String currentUser(HttpServletRequest request) {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication())
+                .map(authentication -> {
+                    if (authentication.getPrincipal() instanceof UserDetails) {
+                        UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+                        return springSecurityUser.getUsername();
+                    } else if (authentication.getPrincipal() instanceof String) {
+                        return (String) authentication.getPrincipal();
+                    }
+                    return null;
+                }).orElse(null);
     }
 
 
